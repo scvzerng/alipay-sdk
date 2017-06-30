cf62b1f60768d5cdc98990415015f35ba046a6e3
Enuqi <1013566157@qq.com>
2017-06-30 10:58:37 +0800

    AlipayOpenServicemarketOrderQueryModel add valid rule

--- src/main/java/com/alipay/api/AlipayObject.java
+++ src/main/java/com/alipay/api/AlipayObject.java
@@ -10,5 +10,5 @@ import java.io.Serializable;
  */
 public abstract class AlipayObject implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/alipay/api/domain/AlipayOpenServicemarketOrderQueryModel.java
+++ src/main/java/com/alipay/api/domain/AlipayOpenServicemarketOrderQueryModel.java
@@ -2,6 +2,9 @@ package com.alipay.api.domain;
 
 import com.alipay.api.AlipayObject;
 import com.alipay.api.internal.mapping.ApiField;
+import io.swagger.annotations.*;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * 用于服务商回查服务市场订单明细信息
@@ -9,6 +12,7 @@ import com.alipay.api.internal.mapping.ApiField;
  * @author auto create
  * @since 1.0, 2017-03-01 16:20:58
  */
+@ApiModel(description = " 用于服务商回查服务市场订单明细信息")
 public class AlipayOpenServicemarketOrderQueryModel extends AlipayObject {
 
 	private static final long serialVersionUID = 7868781215654577436L;
@@ -16,14 +20,18 @@ public class AlipayOpenServicemarketOrderQueryModel extends AlipayObject {
 	/**
 	 * 商户订单ID号
 	 */
+	@ApiModelProperty(notes = " 商户订单ID号")
+	@NotNull(message = "商户订单id号不能为空")
 	@ApiField("commodity_order_id")
 	private String commodityOrderId;
 
 	/**
 	 * 从第几页开始查询
 	 */
+	@ApiModelProperty(notes = " 从第几页开始查询")
+	@NotNull(message = "开始页不能为空")
 	@ApiField("start_page")
-	private String startPage;
+	private Integer startPage;
 
 	public String getCommodityOrderId() {
 		return this.commodityOrderId;
@@ -32,11 +40,11 @@ public class AlipayOpenServicemarketOrderQueryModel extends AlipayObject {
 		this.commodityOrderId = commodityOrderId;
 	}
 
-	public String getStartPage() {
-		return this.startPage;
-	}
-	public void setStartPage(String startPage) {
-		this.startPage = startPage;
+	public Integer getStartPage() {
+		return startPage;
 	}
 
+	public void setStartPage(Integer startPage) {
+		this.startPage = startPage;
+	}
 }
