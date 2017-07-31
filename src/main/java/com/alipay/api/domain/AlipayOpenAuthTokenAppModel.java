33a037d58016b1e2dbe82db170a0984f7b56e198
905629076@qq.com <qzqz12320>
2017-07-31 15:52:42 +0800

    注释增加

--- src/main/java/com/alipay/api/domain/AlipayOpenAuthTokenAppModel.java
+++ src/main/java/com/alipay/api/domain/AlipayOpenAuthTokenAppModel.java
@@ -1,7 +1,11 @@
 package com.alipay.api.domain;
 
+import com.alibaba.fastjson.annotation.*;
 import com.alipay.api.AlipayObject;
-import com.alipay.api.internal.mapping.ApiField;
+import io.swagger.annotations.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * 用应用授权码（app_auth_code）换取或者刷新应用授权令牌(app_auth_token)
@@ -9,48 +13,58 @@ import com.alipay.api.internal.mapping.ApiField;
  * @author auto create
  * @since 1.0, 2016-10-27 16:28:06
  */
+@ApiModel(description = " 用应用授权码（app_auth_code）换取或者刷新应用授权令牌(app_auth_token)")
 public class AlipayOpenAuthTokenAppModel extends AlipayObject {
 
-	private static final long serialVersionUID = 1217736144721624145L;
+    private static final long serialVersionUID = 1217736144721624145L;
 
-	/**
-	 * 授权码，如果grant_type的值为authorization_code。该值必须填写
-	 */
-	@ApiField("code")
-	private String code;
+    /**
+     * 授权码，如果grant_type的值为authorization_code。该值必须填写
+     */
+    @ApiModelProperty(notes = " 授权码，如果grant_type的值为authorization_code。该值必须填写")
+    @Length(min=1, max=40)
+    private String code;
 
-	/**
-	 * authorization_code表示换取app_auth_token。
-refresh_token表示刷新app_auth_token。
-	 */
-	@ApiField("grant_type")
-	private String grantType;
+    /**
+     * authorization_code表示换取app_auth_token。
+     * refresh_token表示刷新app_auth_token。
+     */
+    @JSONField(name = "grant_type", alternateNames = "grantType")
+    @ApiModelProperty(notes = " authorization_code表示换取app_auth_token。refresh_token表示刷新app_auth_token。")
+    @NotNull(message = "grant_type值不能为空")
+    @Length(min=1 ,max =20)
+    private String grantType;
 
-	/**
-	 * 刷新令牌，如果grant_type值为refresh_token。该值不能为空。该值来源于此接口的返回值app_refresh_token（至少需要通过grant_type=authorization_code调用此接口一次才能获取）
-	 */
-	@ApiField("refresh_token")
-	private String refreshToken;
+    /**
+     * 刷新令牌，如果grant_type值为refresh_token。该值不能为空。该值来源于此接口的返回值app_refresh_token（至少需要通过grant_type=authorization_code调用此接口一次才能获取）
+     */
+    @JSONField(name = "refresh_token", alternateNames = "refreshToken")
+    @ApiModelProperty(notes = " 刷新令牌，如果grant_type值为refresh_token。该值不能为空。该值来源于此接口的返回值app_refresh_token（至少需要通过grant_type=authorization_code调用此接口一次才能获取）")
+    @Length(min=1,max=40)
+    private String refreshToken;
 
-	public String getCode() {
-		return this.code;
-	}
-	public void setCode(String code) {
-		this.code = code;
-	}
+    public String getCode() {
+        return this.code;
+    }
 
-	public String getGrantType() {
-		return this.grantType;
-	}
-	public void setGrantType(String grantType) {
-		this.grantType = grantType;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-	public String getRefreshToken() {
-		return this.refreshToken;
-	}
-	public void setRefreshToken(String refreshToken) {
-		this.refreshToken = refreshToken;
-	}
+    public String getGrantType() {
+        return this.grantType;
+    }
+
+    public void setGrantType(String grantType) {
+        this.grantType = grantType;
+    }
+
+    public String getRefreshToken() {
+        return this.refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 
 }
