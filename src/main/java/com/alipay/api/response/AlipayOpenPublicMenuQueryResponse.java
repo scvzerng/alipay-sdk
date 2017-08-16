837e7151bc22e621e76da0c6debd807be08366a4
905629076@qq.com <qzqz12320>
2017-08-16 16:07:28 +0800

    注释增加

--- src/main/java/com/alipay/api/response/AlipayOpenPublicMenuQueryResponse.java
+++ src/main/java/com/alipay/api/response/AlipayOpenPublicMenuQueryResponse.java
@@ -13,37 +13,42 @@ import java.util.List;
 
 /**
  * ALIPAY API: alipay.open.public.menu.query response.
- * 
+ *
  * @author auto create
  * @since 1.0, 2016-12-08 11:57:40
- */@ApiModel(description = " ALIPAY API: alipay.open.public.menu.query response.")
+ */
+@ApiModel(description = " ALIPAY API: alipay.open.public.menu.query response.")
 public class AlipayOpenPublicMenuQueryResponse extends AlipayResponse {
 
-	private static final long serialVersionUID = 1424166512179147572L;
+    private static final long serialVersionUID = 1424166512179147572L;
 
-	/** 
-	 * 一级菜单数组，个数应为1~4个
-	 */@JSONField(name = "menu_content", alternateNames = "menuContent")@ApiModelProperty(notes = " 一级菜单数组，个数应为1~4个")
-	private Object menuContent;
+    /**
+     * 一级菜单数组，个数应为1~4个
+     */
+    @JSONField(name = "menu_content", alternateNames = "menuContent")
+    @ApiModelProperty(notes = " 一级菜单数组，个数应为1~4个")
+    private Object menuContent;
 
-	/**
-	 * 一级菜单数组，个数应为1~4个
-	 */@ApiModelProperty(notes = " 一级菜单数组，个数应为1~4个")
-	private List<ButtonObject> button;
+    /**
+     * 一级菜单数组，个数应为1~4个
+     */
+    @ApiModelProperty(notes = " 一级菜单数组，个数应为1~4个")
+    private List<ButtonObject> button;
 
-	public void setMenuContent(Object menuContent) {
-		this.menuContent = menuContent;
-	}
-	public Object getMenuContent( ) {
-		return this.menuContent;
-	}
+    public void setMenuContent(Object menuContent) {
+        this.menuContent = menuContent;
+    }
 
-	public void setButton(List<ButtonObject> button) {
-		this.button = button;
-	}
+    public Object getMenuContent() {
+        return this.menuContent;
+    }
 
-	public List<ButtonObject> getButton() {
+    public void setButton(List<ButtonObject> button) {
+        this.button = button;
+    }
 
-		return button;
-	}
+    public List<ButtonObject> getButton() {
+
+        return button;
+    }
 }
