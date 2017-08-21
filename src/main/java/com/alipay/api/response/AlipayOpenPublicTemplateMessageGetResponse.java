92b71f8a5fe69ac5a2abda4a695a2c4d90770e40
905629076@qq.com <qzqz12320>
2017-08-21 13:51:35 +0800

    代码修改

--- src/main/java/com/alipay/api/response/AlipayOpenPublicTemplateMessageGetResponse.java
+++ src/main/java/com/alipay/api/response/AlipayOpenPublicTemplateMessageGetResponse.java
@@ -1,46 +1,42 @@
 package com.alipay.api.response;
 
-import com.alibaba.fastjson.annotation.*;
-import com.alipay.api.fastjson.NormalizerJSON;
 import com.alipay.api.internal.mapping.ApiField;
 
 import com.alipay.api.AlipayResponse;
-import io.swagger.annotations.*;
 
 /**
  * ALIPAY API: alipay.open.public.template.message.get response.
  *
  * @author auto create
  * @since 1.0, 2016-12-08 11:44:54
- */@ApiModel(description = " ALIPAY API: alipay.open.public.template.message.get response.")
-@NormalizerJSON(false)
+ */
 public class AlipayOpenPublicTemplateMessageGetResponse extends AlipayResponse {
 
-    private static final long serialVersionUID = 4439562879856576129L;
+    private static final long serialVersionUID = 8289721221645271687L;
 
     /**
      * 消息模板id--商户领取母版后生成的唯一模板id
-     */@ApiModelProperty(notes = " 消息模板id--商户领取母版后生成的唯一模板id")@JSONField(name = "msg_template_id", alternateNames = "msgTemplateId")
+     */
+    @ApiField("msg_template_id")
     private String msgTemplateId;
 
     /**
      * 模板内容
-     */@ApiModelProperty(notes = " 模板内容")
+     */
+    @ApiField("template")
     private String template;
 
     public void setMsgTemplateId(String msgTemplateId) {
         this.msgTemplateId = msgTemplateId;
     }
-
-    public String getMsgTemplateId() {
+    public String getMsgTemplateId( ) {
         return this.msgTemplateId;
     }
 
     public void setTemplate(String template) {
         this.template = template;
     }
-
-    public String getTemplate() {
+    public String getTemplate( ) {
         return this.template;
     }
 
