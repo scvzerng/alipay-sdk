345fcbbce1a5a390ab7ae632965d3c6aefb4ac82
905629076@qq.com <qzqz12320>
2017-08-21 15:16:01 +0800

    注释增加

--- src/main/java/com/alipay/api/domain/ButtonObject.java
+++ src/main/java/com/alipay/api/domain/ButtonObject.java
@@ -2,16 +2,18 @@ package com.alipay.api.domain;
 
 import java.util.List;
 
+import com.alibaba.fastjson.annotation.*;
 import com.alipay.api.AlipayObject;
 import com.alipay.api.internal.mapping.ApiField;
 import com.alipay.api.internal.mapping.ApiListField;
+import io.swagger.annotations.*;
 
 /**
  * 菜单对象模型
  *
  * @author auto create
  * @since 1.0, 2016-10-26 17:43:41
- */
+ */@ApiModel(description = " 菜单对象模型")
 public class ButtonObject extends AlipayObject {
 
 	private static final long serialVersionUID = 2266282688158274318L;
@@ -21,8 +23,7 @@ public class ButtonObject extends AlipayObject {
 当actionType为out时，该参数为用户自定义参数；
 当actionType为tel时，该参数为电话号码。
 该参数最长255个字符，不允许冒号等特殊字符
-	 */
-	@ApiField("action_param")
+	 */@JSONField(name = "action_param", alternateNames = "actionParam")@ApiModelProperty(notes = " 当actionType为link时，该参数为详细链接；当actionType为out时，该参数为用户自定义参数；当actionType为tel时，该参数为电话号码。该参数最长255个字符，不允许冒号等特殊字符")
 	private String actionParam;
 
 	/**
@@ -30,21 +31,17 @@ public class ButtonObject extends AlipayObject {
 out——事件型菜单；
 link——链接型菜单；
 tel——点击拨打电话
-	 */
-	@ApiField("action_type")
+	 */@JSONField(name = "action_type", alternateNames = "actionType")@ApiModelProperty(notes = " 菜单类型：out——事件型菜单；link——链接型菜单；tel——点击拨打电话")
 	private String actionType;
 
 	/**
 	 * 菜单标题，一级菜单不超过4个汉字，子菜单不超过12个汉字
-	 */
-	@ApiField("name")
+	 */@ApiModelProperty(notes = " 菜单标题，一级菜单不超过4个汉字，子菜单不超过12个汉字")
 	private String name;
 
 	/**
 	 * 二级菜单数组，个数应为1~5个
-	 */
-	@ApiListField("sub_button")
-	@ApiField("sub_button")
+	 */@JSONField(name = "sub_button", alternateNames = "subButton")@ApiModelProperty(notes = " 二级菜单数组，个数应为1~5个")
 	private List<SubButton> subButton;
 
 	public String getActionParam() {
